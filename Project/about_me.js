347b4084fef6d77e39fb938ee72a9179e1be9d75
TheLaw30 <Lawr3nce.lu@gmail.com>
2023-10-18 09:14:40 -0700

    poop

--- Project/about_me.js
+++ Project/about_me.js
@@ -1,10 +1,10 @@
 document.getElementById("demo").innerHTML = "Hello JavaScript";
 demo = setInterval(frame, 5);
 
-function frame() {
-  if (/* test for finished */) {
-    clearInterval(demo);
-  } else {
-    /* code to change the element style */ 
-  }
-}
+// function frame() {
+//   if (/* test for finished */) {
+//     clearInterval(demo);
+//   } else {
+//     /* code to change the element style */ 
+//   }
+// }
